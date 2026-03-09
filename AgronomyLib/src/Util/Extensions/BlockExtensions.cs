3c584b01ce83a302a0605ddcc671dc39f492e7d7
DocViviLeandra <docvivileandra@gmail.com>
2026-03-09 17:39:36 -0500

    Add comments

--- AgronomyLib/src/Util/Extensions/BlockExtensions.cs
+++ AgronomyLib/src/Util/Extensions/BlockExtensions.cs
@@ -9,7 +9,11 @@ using Vintagestory.GameContent;
 
 namespace AgronomyLib {
     public static class BlockExtensions {
-
+        /// <summary>
+        /// An extension method allowing a <see cref="Block"/>'s <see cref="BlockCropProperties"/> to be retrieved in a way that can be overridden, rather than via direct access to <see cref="Block.CropProps"/>. This one retrieves it for the general type of block.
+        /// </summary>
+        /// <param name="block">The <see cref="Block"/> this extension method is being invoked on</param>
+        /// <returns>The <see cref="BlockCropProperties"/> of the <see cref="Block"/>. If the <see cref="Block"/> implements <see cref="IBlockProvidesCropProps"/>, it will retrieve the <see cref="BlockCropProperties"/> via the method it provides, instead of direct access.</returns>
         public static BlockCropProperties GetCropProps(this Block block) {
             if (block is IBlockProvidesCropProps cropPropsProvider) {
                 return cropPropsProvider.CropProperties();
@@ -19,10 +23,10 @@ namespace AgronomyLib {
         }
 
         /// <summary>
-        /// An extension method allowing a <see cref="Block"/>'s <see cref="BlockCropProperties"/> to be retrieved in a way that can be overridden, rather than via direct access to <see cref="Block.CropProps"/>.
+        /// An extension method allowing a <see cref="Block"/>'s <see cref="BlockCropProperties"/> to be retrieved in a way that can be overridden, rather than via direct access to <see cref="Block.CropProps"/>. This one retrieves it for the specific Block at the specified position.
         /// </summary>
         /// <remarks>
-        /// AgronomyLib's <see cref="CropPropsTranspilerPatch"/> replaces all references in the vanilla code to <see cref="Block.CropProps"/> from outside a <see cref="Block"/> class with calls to this method.
+        /// AgronomyLib's <see cref="CropPropsTranspilerPatch"/> replaces all references in <see cref="BlockEntitySoilNutrition"/> and <see cref="BlockEntityFarmland"/> to <see cref="Block.CropProps"/> with calls to this method.
         /// </remarks>
         /// <param name="block">The <see cref="Block"/> this extension method is being invoked on</param>
         /// <param name="Api"></param>
