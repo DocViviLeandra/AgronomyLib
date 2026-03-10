f2d462414a231bf49f2e82215200dccc5f256581
DocViviLeandra <docvivileandra@gmail.com>
2026-03-10 12:51:56 -0500

    Partial implementation of crop containers

--- AgronomyLib/src/Block/BlockCropContainerBase.cs
+++ AgronomyLib/src/Block/BlockCropContainerBase.cs
@@ -2,16 +2,77 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
 using Vintagestory.GameContent;
 
 namespace AgronomyLib {
     /// <summary>
     /// A block which, along with its associated block entity, contains a crop to which farmland updates are applied instead of the block. Allows a variety of trickery, such as beanpoles containing bean plants.
     /// </summary>
-    public class BlockCropContainerBase : Block, IBlockProvidesCropProps, IBlockCropGrowth {
+    public abstract class BlockCropContainerBase : BlockContainer, IBlockProvidesCropProps, IBlockCropGrowth, IBlockCropDeath {
+        // TODO: Support multiple crops of the same type growing in a container, to support nurseries and stuff
+
+        public virtual string meshRefsCacheKey => Code.ToShortString() + "meshRefs";
+
+        public virtual int ContainerSlotId => 0;
+
+        public virtual int GetContainerSlotId(BlockPos pos) {
+            return ContainerSlotId;
+        }
+
+        public virtual int GetContainerSlotId(ItemStack containerStack) {
+            return ContainerSlotId;
+        }
+
+        public override void OnLoaded(ICoreAPI api) {
+            base.OnLoaded(api);
+
+            // TODO: load props?
+
+            if (api.Side != EnumAppSide.Client) return;
+            ICoreClientAPI capi = api as ICoreClientAPI;
+
+            interactions = ObjectCacheUtil.GetOrCreate(api, "agronomylib:cropContainerBase", () => {
+                List<ItemStack> cropContainerStacks = new List<ItemStack>();
+
+                
+                foreach(CollectibleObject obj in api.World.Collectibles) {
+                    if (obj is BlockCropContainerBase blc) { // TODO: Additional checks?
+                        cropContainerStacks.Add(new ItemStack(obj));
+                    }
+                }
+
+                var ccStacks = cropContainerStacks.ToArray();
+
+                return new WorldInteraction[] {
+                    // TODO: standard world interactions
+                };
+            });
+        }
+
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer) {
+            return base.GetPlacedBlockInteractionHelp(world, selection, forPlayer).Append(interactions);
+        }
+
+        public override WorldInteraction[] GetHeldInteractionHelp(ItemSlot inSlot) {
+            // TODO: Base held interaction
+            return base.GetHeldInteractionHelp(inSlot);
+        }
+
+        public WorldInteraction[] interactions { get; protected set; }
+
+
+
+        public ItemStack? GetCropStack(ItemStack containerStack) {
+            ItemStack[] stacks = GetContents(api.World, containerStack);
+            int id = GetContainerSlotId(containerStack);
+            return (stacks != null && stacks.Length > 0) ? stacks[Math.Min(stacks.Length - 1, id)] : null;
+        }
+
         public virtual BlockCropProperties CropProperties() => null; // This isn't a crop if there isn't a crop in it!
 
         public virtual BlockCropProperties CropProperties(IWorldAccessor world, BlockPos pos) {
@@ -44,6 +105,25 @@ namespace AgronomyLib {
             }
 
             return be.TryGrowCrop(farmland, currentTotalHours);
+        }
+
+        public virtual bool TryKillCrop(IWorldAccessor world, BlockPos pos, EnumCropStressType deathReason, BlockEntityFarmland? farmland = null) {
+            DoKillCrop(world, pos, deathReason, farmland);
+            return true;
+        }
+
+        public virtual bool DoKillCrop(IWorldAccessor world, BlockPos pos, EnumCropStressType deathReason, BlockEntityFarmland? farmland = null) {
+            BlockEntityCropContainerBase? be = farmland.Api.World.BlockAccessor.GetBlockEntity(farmland.UpPos) as BlockEntityCropContainerBase;
+            if (be == null) {
+                farmland.Api.Logger.Error("BlockCropContainerBase at position {0} cannot find associated BlockEntityCropContainerBase!", farmland.UpPos);
+                return false;
+            }
+
+            return be.DoKillCrop(deathReason, farmland);
+        }
+
+        public virtual bool OnCropDeath(IWorldAccessor world, BlockPos pos, EnumCropStressType deathReason, BlockEntityFarmland? farmland) {
+            return true;
         }
     }
 }
--- AgronomyLib/src/BlockEntity/BlockEntityCropContainerBase.cs
+++ AgronomyLib/src/BlockEntity/BlockEntityCropContainerBase.cs
@@ -6,16 +6,24 @@ using Vintagestory.API.Common;
 using Vintagestory.GameContent;
 
 namespace AgronomyLib.src.BlockEntity {
-    public class BlockEntityCropContainerBase : BlockEntityContainer {
+    public abstract class BlockEntityCropContainerBase : BlockEntityContainer {
+        // TODO: Support multiple crops of the same type growing in a container, to support nurseries and stuff
+
         protected InventoryGeneric inv;
         public override InventoryBase Inventory => inv;
         public override string InventoryClassName => "agronomylib:cropcontainer";
 
-        public ItemSlot cropSlot => inv[0];
-        public Block? containedCrop => inv[0].Itemstack?.Block;
+        protected bool cropAlive;
 
-        public virtual BlockCropProperties? CropProps() => containedCrop?.GetCropProps();
-        public virtual int CurrentStage() => containedCrop?.GetCurrentCropStage(Api.World, Pos) ?? 0;
+        public virtual BlockCropContainerBase? ownBlock => Block as BlockCropContainerBase;
+        public virtual ItemSlot CropSlot => inv[0];
+        public virtual Block? GetContainedCrop() => CropSlot.Itemstack?.Block != null && CropSlot.Itemstack?.Block.GetCropProps() != null ? CropSlot.Itemstack?.Block : null;
+
+        public virtual bool HasCrop => GetContainedCrop() != null;
+        public virtual bool HasLivingCrop => HasCrop && cropAlive;
+
+        public virtual BlockCropProperties? CropProps() => GetContainedCrop()?.GetCropProps();
+        public virtual int CurrentStage() => GetContainedCrop()?.GetCurrentCropStage(Api.World, Pos) ?? 0;
 
         public BlockEntityCropContainerBase() {
             inv = new InventoryGeneric(1, "agronomylib:cropcontainer-0", null, null);
@@ -27,6 +35,7 @@ namespace AgronomyLib.src.BlockEntity {
 
         protected override void OnTick(float dt) {
             // Don't tick inventory contents
+            // TODO: This might be necessary for uprooted crops?
         }
 
         public override void OnBlockBroken(IPlayer byPlayer = null) {
@@ -35,7 +44,9 @@ namespace AgronomyLib.src.BlockEntity {
         }
 
         public virtual bool TryGrowCrop(BlockEntityFarmland farmland, double currentTotalHours) {
-            Block? crop = containedCrop;
+            if (!cropAlive) return false;
+
+            Block? crop = GetContainedCrop();
             if (crop == null) return false;
 
             int currentGrowthStage = crop.GetCurrentCropStage();
@@ -51,20 +62,41 @@ namespace AgronomyLib.src.BlockEntity {
                     bool result = false;
                     foreach (CropBehavior behavior in cropProps.Behaviors) {
                         result = behavior.TryGrowCrop(Api, farmland, currentTotalHours, newGrowthStage, ref handling);
-                        if (handling == EnumHandling.PreventSubsequent) return result;
+                        if (handling == EnumHandling.PreventSubsequent) {
+                            MarkDirty(true);
+                            return result;
+                        }
                     }
 
-                    if (handling == EnumHandling.PreventDefault) return result;
+                    if (handling == EnumHandling.PreventDefault) {
+                        MarkDirty(true);
+                        return result;
+                    }
                 }
 
-                cropSlot.Itemstack = new ItemStack(nextBlock, 1);
+                CropSlot.Itemstack = new ItemStack(nextBlock, 1);
 
                 // There's one fewer update than growth stages, since we start on the first one already
                 farmland.ConsumeNutrients(cropProps.RequiredNutrient, cropProps.NutrientConsumption / Math.Max(1, cropProps.GrowthStages - 1));
+                MarkDirty(true);
                 return true;
             }
 
             return false;            
+        }
+
+        public virtual bool DoKillCrop(EnumCropStressType deathReason, BlockEntityFarmland farmland = null) {
+            if (!cropAlive) return false;
+
+            cropAlive = false;
+            if (farmland != null) {
+                // Since the block isn't actually being broken, we need to manually call OnCropBlockBroken()
+                farmland.OnCropBlockBroken();
+            }
+
+            MarkDirty(true);
+
+            return true;
         }
     }
 }
